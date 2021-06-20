35211444e4706e1b5e2f574dd78eddfa6f22bb84
seobookpro <79488264+seobookpro@users.noreply.github.com>
2021-06-21 00:22:35 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<!-- Add Header Lang Default + Theme Options to Change it from the SEO Book Pro Nightly - Alpha Options Dashboard -->
+<html lang="en">
+<!-- Start The Header Metas -->
+<head>
+<!-- Add Char, MetaD, OG Metas, Favicon, Canonical, Remove some WP Meta from the Header -->
+<!-- Add Header Meta Title Include the SEO Module for Home, Single, Page, CPT, Other is_home -->
+<title>SEO BOOK PRO Nightly - Alpha v.0.0.3</title>
+  
+  
+<!-- Start and Add WP Head -->
+<?php wp_head();?>
+<!-- End Adding WP Head -->
+<!-- End The Header Metas -->
+</head>
+<body>
