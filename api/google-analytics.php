3dda20fc4cd9ec3f4018698964d29d9d6731ebe5
seobookpro <79488264+seobookpro@users.noreply.github.com>
2021-07-09 06:53:11 +0300

    Google Analytics
    Global site tag (gtag.js) Use this if you're using a website builder or CMS-hosted site
    Add the Analytics tag to your website to begin seeing data in your property.
    Copy the global site tag into the <head> section of your HTML. Or, if you use a website builder (e.g. WordPress, Shopify, etc), copy the global site tag into your website builder’s custom HTML field.

--- api/google-analytics.php
+++ api/google-analytics.php
@@ -1,1 +1,14 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src=""https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX""></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-XXXXXXXXXX');
+</script>
+
+<!-- Add Themplate Options to add the ID and Get Verification for GA/GA4/Tag Manager Scripts Working -->
+<!-- Global site tag (gtag.js) Use this if you're using a website builder or CMS-hosted site  -->
+<!-- Add the Analytics tag to your website to begin seeing data in your property.  -->
+<!-- Copy the global site tag into the <head> section of your HTML. Or, if you use a website builder (e.g. WordPress, Shopify, etc), copy the global site tag into your website builder’s custom HTML field. -->
