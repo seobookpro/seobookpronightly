ee376f82eed90821c6075f1c41953617f253e8b7
seobookpro <79488264+seobookpro@users.noreply.github.com>
2021-07-09 06:49:58 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,22 @@
 <?php
 // All  SEO Book Pro Nightly Alpha Functions with Documentation and Information
+// Register Default WordPress codex Functions for
+// Custom Menus, Default Widgets, Sidebar/s and in Content Widgets
+// Add Featured Image Support
+// Exclude WP EMoji
+// Disable Blocks Editor and Enable Default WP Editor ( WYSIWYG )
+// Integrate TinyMSE - Custom Additonal Menu Options for Page Content Edit
+// Register Themplate Options
+// Register SEO Functions
+// Register the Structured Data - JSON-LD - Schema.org and Microdata
+// Add more Features
+// Enable Custom Exerpt 
+// Enable Tag limist in single post
+// Register and add theme options for Custom Post types
+// Register Folder with Advanced SEO Fields
+// Register Custom Meta Box 
+/// - VideoObject Custom Meta Box - Validated Rich Snippet
+/// - FAQPage Custom Meta Box - Rich Snippet + Valid Wrap + Repeater + Help Section 
+/// -- The FAQ Loop Wrapped in Pure HTML - <details><summary>The FAQ Title</summary><p>The Answer here</p></details> - Demo
 
 ?>
